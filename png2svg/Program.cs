b691cf0d4132b933e7af06efd2b5c455288bf2cb
4UPanElektryk <80508394+4UPanElektryk@users.noreply.github.com>
2024-09-24 11:26:44 +0200

    k

--- png2svg/Program.cs
+++ png2svg/Program.cs
@@ -16,18 +16,87 @@ namespace png2svg
 		{
 			public int counted;
 			public Color color;
-		} 
+		}
 		static void Main(string[] args)
 		{
-			if(args.Length < 2)
+			if (args.Length < 3)
 			{
 				return;
 			}
 			string inputpng = args[0];
 			string outputsvg = args[1];
+			string format = args[2];
 			Stopwatch stopwatch = new Stopwatch();
 			stopwatch.Start();
 			Bitmap bitmap = new Bitmap(Image.FromFile(inputpng));
+			int oheight = int.Parse(args[2].Split(':')[0]), owidth = int.Parse(args[2].Split(':')[1]);
+
+
+			List<string> strings = new List<string>
+			{
+				$"<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"100%\" height=\"100%\" viewBox=\"0 0 {bitmap.Width} {bitmap.Height}\">"
+			};
+			int[] ints = { owidth, oheight };
+			Color[,] bgcols = new Color[owidth, oheight];
+			foreach (var item in printAllDivisors(ints))
+			{
+				int width = owidth / item;
+				int height = oheight / item;
+				bgcols = new Color[owidth, oheight];
+				Bitmap[,] bitmaps = DevideImage(bitmap, width, height);
+				for (int x = 0; x < width; x++)
+				{
+					for (int y = 0; y < height; y++)
+					{
+						Console.Write($"Analising image [{x}:{y}] Col: ");
+						bgcols[x, y] = CalculateAverageSector(bitmaps[x, y]);
+						strings.Add($"<rect x=\"{bitmap.Width / width * x}\" y=\"{bitmap.Height / height * y}\" width=\"{bitmap.Width / width}\" height=\"{bitmap.Height / height}\" style=\"fill:{RGBConverter(bgcols[x, y])};stroke-width:1\" />");
+					}
+				}
+			}
+			Console.WriteLine($"Filling image");
+			for (int y = 0; y < bitmap.Height; y++)
+			{
+				for (int x = 0; x < bitmap.Width; x++)
+				{
+					if (bitmap.GetPixel(x, y) != bgcols[x / (bitmap.Width / owidth) , y / (bitmap.Height / oheight)])
+					{
+						strings.Add("<rect width=\"1\" x=\"" + x + "\" y=\"" + y + "\" height=\"1\" style=\"fill:" + RGBConverter(bitmap.GetPixel(x, y)) + ";stroke-width:1\" />");
+					}
+				}
+			}
+			strings.Add("</svg>");
+			File.WriteAllLines(outputsvg, strings);
+			Console.WriteLine("task took: " + stopwatch.ElapsedMilliseconds / 1000.0);
+		}
+		private static Bitmap[,] DevideImage(Bitmap bitmap, int width, int height)
+		{
+			Bitmap[,] bit = new Bitmap[width, height];
+			for (int x = 0; x < width; x++)
+			{
+				for (int y = 0; y < height; y++)
+				{
+					bit[x, y] = new Bitmap(bitmap.Width/width, bitmap.Height/height);
+				}
+			}
+			for (int x = 0; x < bitmap.Width; x++) {
+				for (int y = 0; y < bitmap.Height; y++) {
+
+					int bitmapx = x / (bitmap.Width / width);
+					int bitmapy = y / (bitmap.Height / height);
+					//Console.WriteLine($"Bitmap @ [{x}, {y}]");
+					Bitmap correctone = bit[bitmapx, bitmapy];
+					//Console.WriteLine($"Bitmap info:  Width: {correctone.Width} Height: {correctone.Height}");
+					int bitx = x % (bitmap.Width / width);
+					int bity = y % (bitmap.Height / height);
+					correctone.SetPixel(bitx, bity, bitmap.GetPixel(x,y));
+					bit[bitmapx, bitmapy] = correctone;
+				}
+			}
+			return bit;
+		}
+		private static Color CalculateAverageSector(Bitmap bitmap)
+		{
 			List<Color> colors = new List<Color>();
 			for (int y = 0; y < bitmap.Height; y++)
 			{
@@ -73,38 +142,54 @@ namespace png2svg
 					col = item.color;
 				}
 			}
-			List<string> strings= new List<string> 
-			{
-				"<svg height=\"" + bitmap.Height + "\" width=\""+bitmap.Width+"\" style=\"background-color: "+ RGBConverter(col) +";\">"
-			};
-			for (int y = 0; y < bitmap.Height; y++)
-			{
-				for (int x = 0; x < bitmap.Width; x++)
-				{
-					if (bitmap.GetPixel(x, y) != col)
-					{
-						strings.Add("<rect width=\"1\" x=\"" + x + "\" y=\"" + y + "\" height=\"1\" style=\"fill:" + RGBConverter(bitmap.GetPixel(x,y)) + ";stroke-width:1\" />");
-						strings.Add("<!-- https://github.com/4UPanElektryk/png2svg -->")
-					}
-				}
-			}
-			strings.Add("</svg>");
-			File.WriteAllLines(outputsvg,strings);
-			Console.WriteLine("task took: " + stopwatch.ElapsedMilliseconds/1000.0);
+			Console.WriteLine(RGBConverter(col));
+			return col;
 		}
 		private static string RGBConverter(Color c)
 		{
-			string rtn = string.Empty;
-			try
+			return $"#{c.R:X2}{c.G:X2}{c.B:X2}";
+		}
+
+		static int gcd(int a, int b)
+		{
+			if (a == 0)
+				return b;
+			return gcd(b % a, a);
+		}
+
+		// Function to print all the
+		// common divisors
+		static int[] printAllDivisors(int[] arr)
+		{
+			// Variable to find the gcd
+			// of N numbers
+			int g = arr[0];
+
+			// Set to store all the
+			// common divisors
+			HashSet<int> divisors = new HashSet<int>();
+
+			// Finding GCD of the given
+			// N numbers
+			for (int i = 1; i < arr.Length; i++)
 			{
-				rtn = "rgb(" + c.R.ToString() + "," + c.G.ToString() + "," + c.B.ToString() + ")";
-			}
-			catch (Exception ex)
-			{
-				//doing nothing
+				g = gcd(arr[i], g);
 			}
 
-			return rtn;
+			// Finding divisors of the
+			// HCF of n numbers
+			for (int i = 1; i * i <= g; i++)
+			{
+				if (g % i == 0)
+				{
+					divisors.Add(i);
+					if (g / i != i)
+						divisors.Add(g / i);
+				}
+			}
+
+			// Print all the divisors
+			return divisors.ToArray();
 		}
 	}
 }
